b00bdef98da254b093e7d4f75e4239e25ac20131
Francesco <francesco.filippini8@studio.unibo.it>
2024-02-02 09:35:09 +0100

    Added Controller

---
+++ src/main/java/project/core/Controller.java
@@ -0,0 +1,9 @@
+package project.core;
+
+/**
+ * Interface for the controller of the database's app.
+ */
+
+public interface Controller {
+    
+}
---
+++ src/main/java/project/core/ControllerImpl.java
@@ -0,0 +1,9 @@
+package project.core;
+
+/**
+ * Implementation of Controller. It allows interactions between Database and view.
+ */
+
+public class ControllerImpl implements Controller {
+    
+}
