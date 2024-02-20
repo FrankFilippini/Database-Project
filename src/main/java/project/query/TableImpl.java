9f36807781e38607724316e65a467366c17ed1fc
Francesco <francesco.filippini8@studio.unibo.it>
2024-02-20 10:59:18 +0100

    Implemented TableImpl

--- src/main/java/project/query/TableImpl.java
+++ src/main/java/project/query/TableImpl.java
@@ -4,22 +4,32 @@ import java.util.List;
 
 public class TableImpl implements Table{
 
+    private List<List<String>> records;
+
+    /**
+     * Constructor
+     * @param records given list of records
+     */
+    public TableImpl(List<List<String>> records) {
+        this.records = records;
+    }
+
     @Override
     public List<String> getColumnsName() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'getColumnsName'");
+        return this.records.get(0);
     }
 
     @Override
     public List<List<String>> getRecords() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'getRecords'");
+        return this.records;
     }
 
     @Override
     public List<String> getRecord(int index) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'getRecord'");
+        if (index < 1 || index >= records.size()) {
+            throw new IndexOutOfBoundsException("Wrong Index!!!");
+        }
+        return this.records.get(index);
     }
     
 }
