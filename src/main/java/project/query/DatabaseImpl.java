3f7ae5201dcf637bde6d5f0fb15773538b19cd11
Francesco <francesco.filippini8@studio.unibo.it>
2024-02-20 10:40:11 +0100

    Implemented interface Database

--- src/main/java/project/query/DatabaseImpl.java
+++ src/main/java/project/query/DatabaseImpl.java
@@ -1,5 +1,25 @@
 package project.query;
 
-public class DatabaseImpl {
+import java.util.List;
+
+public class DatabaseImpl implements Database {
+
+    @Override
+    public List<String> getTableNames() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getTableNames'");
+    }
+
+    @Override
+    public Table getTable(String TableName) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getTable'");
+    }
+
+    @Override
+    public boolean tryInsertRecord(String TableName, List<String> record) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'tryInsertRecord'");
+    }
     
 }
