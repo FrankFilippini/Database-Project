2023f3c735209638f4b99830b254db03ea62220e
Francesco <francesco.filippini8@studio.unibo.it>
2024-02-27 10:29:09 +0100

    Implemented method getTable() in DatabaseImpl

--- src/main/java/project/query/DatabaseImpl.java
+++ src/main/java/project/query/DatabaseImpl.java
@@ -1,10 +1,14 @@
 package project.query;
 
+import java.util.Collection;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Optional;
+import java.util.stream.Stream;
 import java.sql.Statement;
 import java.sql.Connection;
 import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
 import java.sql.SQLException;
 
 public class DatabaseImpl implements Database {
@@ -27,7 +31,7 @@ public class DatabaseImpl implements Database {
         try {
             this.statement = this.connection.createStatement();
             this.resultSet = statement.executeQuery(query);
-            return getList();
+            return getList();                               //get a list of result from a ResultSet
         } catch(SQLException e) {
             return List.of();
         }
@@ -43,8 +47,43 @@ public class DatabaseImpl implements Database {
 
     @Override
     public Table getTable(String TableName) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'getTable'");
+        String query = "SELECT * FROM" + TableName;
+        try {
+            this.statement = this.connection.createStatement();
+            this.resultSet = statement.executeQuery(query);
+            List<String> columnsNames = getColumnsNames(this.resultSet.getMetaData());
+            List<List<String>> list = new LinkedList<>();
+            list.add(columnsNames);
+            list.addAll(getListFromResultSet());
+            return null;
+        } catch (SQLException e) {
+            return null;
+        }
+    }
+
+    private List<String> getColumnsNames(ResultSetMetaData metaData) throws SQLException {
+        int count = metaData.getColumnCount();
+        return Stream.iterate(1,i -> i <= count, i -> i+1)
+                    .map(i -> {try {return metaData.getColumnName(i);} catch(SQLException e) { return "";}})
+                    .toList();
+    }
+
+    private List<List<String>> getListFromResultSet() throws SQLException {
+        final List<List<String>> list = new LinkedList<>();
+        while(this.resultSet.next()) {
+            list.add(getRecord());
+        }
+        return list;
+
+    }
+
+    private List<String> getRecord() throws SQLException {
+        int count = this.resultSet.getMetaData().getColumnCount();
+        final List<String> record = new LinkedList<>();
+        for (int i=1; i<=count; i++){
+            record.add(Optional.ofNullable(this.resultSet.getObject(i)).map(Object :: toString).orElse(""));
+        }
+        return record;
     }
 
     @Override
