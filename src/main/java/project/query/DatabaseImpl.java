205af23fb0688c1234b84167ac6ee33a24d1566c
Francesco <francesco.filippini8@studio.unibo.it>
2024-02-27 09:36:30 +0100

    Implemented method getTableNames() in DatabaseImpl

--- src/main/java/project/query/DatabaseImpl.java
+++ src/main/java/project/query/DatabaseImpl.java
@@ -1,11 +1,17 @@
 package project.query;
 
+import java.util.LinkedList;
 import java.util.List;
+import java.sql.Statement;
 import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
 public class DatabaseImpl implements Database {
 
     private final Connection connection;
+    private Statement statement;
+    private ResultSet resultSet;
 
     /***
      * Constructor
@@ -17,8 +23,22 @@ public class DatabaseImpl implements Database {
 
     @Override
     public List<String> getTableNames() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'getTableNames'");
+        String query = "SELECT table_name" + "table_schema = 'bathhouse'";
+        try {
+            this.statement = this.connection.createStatement();
+            this.resultSet = statement.executeQuery(query);
+            return getList();
+        } catch(SQLException e) {
+            return List.of();
+        }
+    }
+
+    private List<String> getList() throws SQLException {
+        final List<String> list = new LinkedList<>();
+        while(this.resultSet.next()) {
+            list.add(this.resultSet.getString("table_name"));
+        }
+        return list;
     }
 
     @Override
