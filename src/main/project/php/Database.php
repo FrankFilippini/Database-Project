21016e3d985fa54125b04a4d14b875239932207f
Francesco <francesco.filippini8@studio.unibo.it>
2024-08-21 12:41:08 +0200

    Update creation of db

--- src/main/project/php/Database.php
+++ src/main/project/php/Database.php
@@ -19,11 +19,11 @@ class Database {
     }
 
     // O1
-    function clientRegister($username, $CF, $nome, $cognome, $email, $numeroTelefono, $password) {
+    function clientRegister($CF, $nome, $cognome, $email, $numeroTelefono, $password) {
         $profileName = 'default.svg';
-        $stmt = $this->conn->prepare('INSERT INTO CLIENTI (codiceCliente, CF, nome, cognome, email, numeroTelefono, password)
-                                      VALUES (?, ?, ?, ?, ?, ?, ?)');
-        $stmt->bind_param('issssis', $codiceCliente, $CF, $nome, $cognome, $email, $numeroTelefono, $password);
+        $stmt = $this->conn->prepare('INSERT INTO CLIENTI (CF, nome, cognome, email, numeroTelefono, password)
+                                      VALUES (?, ?, ?, ?, ?, ?)');
+        $stmt->bind_param('ssssis', $CF, $nome, $cognome, $email, $numeroTelefono, $password);
 
         if ($stmt->execute()) {
             return true;
@@ -32,11 +32,11 @@ class Database {
     }
 
     // O2
-    function staffRegister($codiceStaff, $idTurno, $CF, $nome, $cognome, $email, $password) {
+    function staffRegister($idTurno, $CF, $nome, $cognome, $email, $password) {
         $profileName = 'default.svg';
-        $stmt = $this->conn->prepare('INSERT INTO MEMBRI (codiceStaff, idTurno, CF, nome, cognome, email, password)
-                                        VALUES (?, ?, ?, ?, ?, ?, ?)');
-        $stmt->bind_param('iisssss', $codiceStaff, $idTurno, $CF, $nome, $cognome, $email, $password);
+        $stmt = $this->conn->prepare('INSERT INTO MEMBRI (idTurno, CF, nome, cognome, email, password)
+                                        VALUES (?, ?, ?, ?, ?, ?)');
+        $stmt->bind_param('isssss', $idTurno, $CF, $nome, $cognome, $email, $password);
 
         if ($stmt->execute()) {
             return true;
@@ -46,7 +46,7 @@ class Database {
 
     //O3
 
-    function bookUmbrella($codicePrenotazione, $codiceCliente, $dataInizio, $dataFine, $mese, $codiceLettino, $codicePedalo, $numeroTavolo) {
+    function bookUmbrella($codiceCliente, $dataInizio, $dataFine, $mese, $codiceLettino, $codicePedalo, $numeroTavolo) {
         $stmt = $this->conn->prepare('SELECT o.codiceOmbrellone
             FROM OMBRELLONI o
             WHERE o.codiceOmbrellone NOT IN (
@@ -70,7 +70,7 @@ class Database {
             $stmt->execute();
     
             $stmt = $this->conn->prepare('
-                INSERT INTO TIPOLOGIE (codicePrenotazione, codiceOmbrellone, codiceLettino, codicePedalo, numeroTavolo)
+                INSERT INTO TIPOLOGIE (codiceOmbrellone, codiceLettino, codicePedalo, numeroTavolo)
                 VALUES ((SELECT MAX(codicePrenotazione) FROM PRENOTAZIONI), ?, ?, ?, ?);
             ');
             $stmt->bind_param('iiii', $codiceOmbrellone, $codiceLettino, $codicePedalo, $numeroTavolo);
@@ -102,10 +102,10 @@ class Database {
     }
 
     //O5
-    function addEvent($codiceEvento, $codiceStaff, $dataInizio, $dataFine, $numeroPosti) {
-        $stmt = $this->conn->prepare('INSERT INTO EVENTI (codiceEvento, codiceStaff, dataInizio, dataFine, numeroPosti)
+    function addEvent($codiceStaff, $dataInizio, $dataFine, $numeroPosti) {
+        $stmt = $this->conn->prepare('INSERT INTO EVENTI (codiceStaff, dataInizio, dataFine, numeroPosti)
             VALUES (?, ?, ?, ?, ?)'); 
-        $stmt->bind_param('iissi', $codiceEvento, $codiceStaff, $dataInizio, $dataFine, $numeroPosti);
+        $stmt->bind_param('issi', $codiceStaff, $dataInizio, $dataFine, $numeroPosti);
          if($stmt->execute()) {
              return true;
          } else {
@@ -121,10 +121,10 @@ class Database {
 
     //O7
 
-    function insertReview($codiceRecensione, $codiceCliente, $codiceStaff, $mese, $valutazione) {
-        $stmt = $this->conn->prepare('INSERT INTO RECENSIONI (codiceRecensione, codiceCliente, codiceStaff, mese, valutazione)
-            VALUES (?, ?, ?, ?, ?)');
-        $stmt->bind_param('iiisi', $codiceRecensione, $codiceCliente, $codiceStaff, $mese, $valutazione);
+    function insertReview($codiceCliente, $codiceStaff, $mese, $valutazione) {
+        $stmt = $this->conn->prepare('INSERT INTO RECENSIONI (codiceCliente, codiceStaff, mese, valutazione)
+            VALUES (?, ?, ?, ?)');
+        $stmt->bind_param('iisi',$codiceCliente, $codiceStaff, $mese, $valutazione);
         if($stmt->execute()) {
             return true;
         } else {
