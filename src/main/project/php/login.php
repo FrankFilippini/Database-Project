1a47e153b8f260950f1cc8ebb9dd789789e058df
Alessandro <alessandro.verna@studio.unibo.it>
2024-08-25 18:23:25 +0200

    Fix user registration

--- src/main/project/php/login.php
+++ src/main/project/php/login.php
@@ -1,19 +1,21 @@
 <?php
     require_once('config.php');
 
-    if (isset($_POST['email'])) {
-        link_to('reservation.php');
-    }
     if ($_GET['page'] === 'signin-client') {
         $templateParams['title'] = 'Sign In - Client';
         $templateParams['css'][] = 'signin.css';
         $templateParams['page'] = 'signin_client.php';
+        if (isset($_POST['email'])) { //verificare che le credenziali siano corrette
+            link_to('reservation.php');
+        }
     } else if($_GET['page'] === 'signup-client') {
         $templateParams['title'] = 'Sign Up - Client';
         $templateParams['css'][] = 'signup.css';
         $templateParams['page'] = 'signup_client.php';
         if (isset($_POST['pwd1'])) {
-            $db->clientRegister($_POST['CF'],$_POST['name'],$_POST['surname'],$_POST['email'],$_POST['phone'],$_POST['pwd1']);
+            if($db->clientRegister($_POST['CF'],$_POST['name'],$_POST['surname'],$_POST['email'],$_POST['phone'],$_POST['pwd1'])){
+                link_to('reservation.php');
+            }
         }
     }  else if($_GET['page'] === 'signin-staff') {
         $templateParams['title'] = 'Sign In - Staff';
