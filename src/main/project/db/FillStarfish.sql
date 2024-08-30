f5390ec08ce5af3505d1ba275da4cad2dfbea75f
Alessandro <alessandro.verna@studio.unibo.it>
2024-08-30 22:41:00 +0200

    Fix foreign key for MEMBRI

---
+++ src/main/project/db/FillStarfish.sql
@@ -0,0 +1,21 @@
+USE `Starfish`;
+
+-- AMMINISTRATORE
+INSERT INTO `AMMINISTRATORI`(`codiceStaff`, `CF`, `nome`, `cognome`, `email`, `password`) 
+VALUES (0,'MRORSI05X31C504K','Mario','Rossi','mario.rossi@admin.starfish.com','test');
+
+-- AGENDA DEI TURNI
+INSERT INTO `AGENDE_DEI_TURNI`(`mese`, `codiceStaff`)
+VALUES (6, 0);
+
+-- TURNO DI LAVORO
+INSERT INTO `TURNI_DI_LAVORO`(`giorno`, `oraInizio`, `oraFine`, `mese`) 
+VALUES (5, TIME '08:00:00', TIME '13:00:00', 6);
+
+-- MEMBRI
+INSERT INTO `MEMBRI`(`CF`, `IdTurno`, `nome`, `cognome`, `email`, `password`) 
+VALUES ('SLVSPS02R24E123W', 1,'Salvatore','Esposito','salvatore.esposito@starfish.com','test');
+
+-- CLIENTE
+INSERT INTO `CLIENTI`(`numeroTelefono`, `CF`, `nome`, `cognome`, `email`, `password`)
+VALUES (1234567890,'NCLNRI99F10G104Q','Nicola','Neri','nicola.neri@gmail.com','test');
